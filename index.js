d29e574dd1ba0fa031e8cd32eb2dcbb752ea203d
haywah27 <hayleywahlroos15@gmail.com>
2021-01-17 19:23:29 -0800

    initializing questions

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'projectName',
+      },
+      {
+        type: 'input',
+        message: 'Enter a description of your application: ',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'What are the installation instructions for the app?',
+        name: 'install',
+      },
+      {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'test',
+      },
+      {
+        type: 'list',
+        message: 'Which license does this project need?',
+        choices: ['Public Domain', 'Permissive', 'LGPL', 'Copyleft', 'Proprietary'],
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'What is a good email to reach you at?',
+        name: 'email',
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -13,3 +60,14 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// sections:
+// Description, 
+// Table of Contents, 
+// Installation, 
+// Usage, 
+// License, 
+// Contributing, 
+// Tests,
+// Questions
