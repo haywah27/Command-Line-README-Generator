cdb57aa313873a2ce60b7be9a490571d1904577a
haywah27 <hayleywahlroos15@gmail.com>
2021-01-21 17:52:28 -0800

    updating readme through node functionality

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Command-Line-README-Generator
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+# Command Line README Generator
+
+## Description
+Using the command line in the integrated terminal, a user can quickly and easily create a README file. This is done using a JavaSCript file containing code for how to handle answers form the user. The questions asked are handled using the Inquirer JSON package. Node is used to invoke the relative JavaScript file. After the questions are answered, fs is used to write a new README.md file, in the repo folder, containing all the provided information
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+Clone this repo and open an integrated terminal on any file from this repository (ex: index.js).
+
+## Usage
+A user needs to access the README questions by invoking the index.js file in an integrated terminal. Using an integrated terminal, input “node index.js” and follow the prompt messages.
+
+## License
+This is free and unencumbered software released into the public domain through the [The Unlicense](http://unlicense.org/)
+
+## Contributing
+Pull requests are the best way to propose changes to the codebase (we use [GitHub Flow](https://guides.github.com/introduction/flow/index.html)). We actively welcome your pull requests:
+1. Fork the repo and create your branch from master. 
+2. If you've added code that should be tested, add tests. 
+3. If you've changed APIs, update the documentation. 
+4. Ensure the test suite passes. 
+5. Make sure your code lints. 
+6. Issue that pull request!
+
+## Tests
+No current tests available.
+
+## Questions 
+* Link to [GitHub](https://github.com/HayWah27)
+* Email: N/A for now.
+* Contact Instructions: 
+  * Currently unavailable.
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 // Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const markdown = require('./generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdown = require("./generateMarkdown");
 
 // array of questions for user input
 const questions = [
     {
-        type: 'input',
-        message: 'What is your repository/ project name?',
-        name: 'projectName',
+        type: "input",
+        message: "What is your repository/ project name?",
+        name: "projectName",
       },
       {
-        type: 'input',
-        message: 'Enter a description of your application: ',
-        name: 'description',
+        type: "input",
+        message: "Enter a description of your application: ",
+        name: "description",
       },
       {
-        type: 'input',
-        message: 'What are the steps required to install your project?',
-        name: 'install',
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "install",
       },
       {
-        type: 'input',
-        message: 'Provide instructions for how to use the app',
-        name: 'usage',
+        type: "input",
+        message: "Provide instructions for how to use the app",
+        name: "usage",
       },
       {
-        type: 'input',
-        message: 'What are the contribution guidelines?',
-        name: 'contribution',
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contribution",
       },
       {
-        type: 'input',
-        message: 'What are the test instructions?',
-        name: 'test',
+        type: "input",
+        message: "What are the test instructions?",
+        name: "test",
       },
       {
-        type: 'list',
-        message: 'Which license does this project need?',
-        choices: ['No License', 'The Unlicense', 'Apache', 'MIT', 'GNU GPLv3'],
-        name: 'license',
+        type: "list",
+        message: "Which license does this project need?",
+        choices: ["No License", "The Unlicense", "Apache", "MIT", "GNU GPLv3"],
+        name: "license",
       },
       {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'github',
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
       },
       {
-        type: 'input',
-        message: 'What is a good email to reach you at?',
-        name: 'email',
+        type: "input",
+        message: "What is a good email to reach you at?",
+        name: "email",
       },
       {
-        type: 'input',
-        message: 'Provide instructions on how to reach you with additional questions?',
-        name: 'contactInstuct',
+        type: "input",
+        message: "Provide instructions on how to reach you with additional questions?",
+        name: "contactInstuct",
       }
 ];
 
@@ -63,7 +63,7 @@ const questions = [
 function writeToFile(fileName, data) {
     
     fs.writeFile(fileName, data, err =>
-    err ? console.error(err) : console.log('Success! New README generated!'));
+    err ? console.error(err) : console.log("Success! New README generated!"));
 
 }
 
@@ -76,7 +76,7 @@ function init() {
             badge = markdown.renderBadge(answers.license);
             badgeLink = markdown.renderLink(answers.license);
             licenseSection = markdown.renderSection(answers.license);
-            writeToFile('log.txt', markdown.markdown(answers, badge, badgeLink, licenseSection));
+            writeToFile("README.md", markdown.markdown(answers, badge, badgeLink, licenseSection));
         })
     
 }
